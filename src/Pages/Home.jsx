b5d84e5bea144971cff2578585bc0e34338ce682
Mlouise99 <mwubahalouise99@gmail.com>
2024-04-23 11:47:14 +0200

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -8,10 +9,16 @@ const Home = () => {
           Jane doe
         </p>
       </div>
-      <div className='flex flex-col p-4'>
-        <a href="#home" className='m-2 text-blue-400'>Home</a>
-        <a href="#about" className='m-2 text-blue-400'>About me</a>
-        <a href="#contact" className='m-2 text-blue-400'>Contact me</a>
+      <div className='flex  p-4 gap-10' >
+        <div className=' flex flex-col '>
+        <Link to="/" className='m-2 text-blue-400'> Home</Link> 
+        <Link to="/about" className='m-2 text-blue-400'> About me</Link>
+        <Link to="/contact" className='m-2 text-blue-400'> Contact me</Link>
+        </div>
+        <div className='bg-yellow-200 justify-center items-center border w-full border-black h-96' >
+
+</div>
+        
       </div>
       
     </div>
